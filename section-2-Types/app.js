dcc533c23517e5fcdd61564bdb1602b56070a1d5
Kofi Bamfo Quaicoe <33463657+bamfokbq@users.noreply.github.com>
2023-03-14 14:47:22 +0000

    types

--- section-2-Types/app.js
+++ section-2-Types/app.js
@@ -1,1 +1,7 @@
-console.log("Template");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 10;
+var num2 = 9.5;
+var result = add(num1, num2);
+console.log(result);
--- section-2-Types/app.ts
+++ section-2-Types/app.ts
@@ -1,1 +1,10 @@
-console.log("Template");
+function add(n1:number, n2:number) { 
+    return n1 + n2;
+}
+
+const num1 = 10;
+const num2 = 9.5
+
+const result = add(num1, num2);
+
+console.log(result);
